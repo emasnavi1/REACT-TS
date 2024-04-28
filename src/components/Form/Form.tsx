a98754a9b06629d24b307761a321ba4de9b35219
emasnavi1 <emasnavi1@gmail.com>
2024-04-28 01:05:06 -0400

    Added for event submit event

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,3 +1,5 @@
+import { FormEvent } from "react";
+
 export default function Form() {
   // This is what you typed in orer to create the contents of the first form: "div>mb-3>label.form-label+input.form-control"
   // after typing the above and pressig the tab, the markup will create a div, with its contents as is available below. This is
@@ -5,11 +7,17 @@ export default function Form() {
   // similarly, for creating the submit button, you did this: "button.btn.btn-primary" this means I want to place a button here
   // with the class of btn and btn-primary
 
-  // PLEase also note the margin areound the forms, that is beceuse you added a global margin of 220 padding un er the index.css, and
+  // Please also note the margin areound the forms, that is beceuse you added a global margin of 220 padding un er the index.css, and
   // imported index.css in the App.tsx
 
+  // The preventDefault is here to prevent the from resetting the page and reloading the page
+  const handleSubmitEvent = (event: FormEvent) => {
+    event.preventDefault();
+    console.log("Submitted");
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmitEvent}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
